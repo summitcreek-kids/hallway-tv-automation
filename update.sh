c659ac2b5d946cbc5ac72e5e634cbf37db481836
Summitcreek Kids <kids@summitcreek.com>
2018-11-03 21:59:49 -0500

    Fixed script so it actually works.

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ git pull
 
 # Rebuild all scripts
 for file in $(ls *.applescript) ; do
-  /usr/bin/osacompile -o ${file}.scptd file
+  echo "Compiling ${file}"
+  /usr/bin/osacompile -o $(basename $file .applescript).scptd $file
 done
 
 # Update crontab
